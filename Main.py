0f76337cc9288d82d747c688115fc418b97d05f9
Luxaas <lucas@kloos.nl>
2023-12-15 09:29:27 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,10 +5,10 @@ def NewImg(xt, yt, zt, punten, oogx, oogy, oogz):
     from PIL import Image
     from Functions import PixCalc, Prep
     tictot=timeit.default_timer()
-    ImgWidth, ImgHeight = 1000, 1000
+    ImgWidth, ImgHeight = 750, 750
         
     Pixels = np.empty([ImgHeight, ImgWidth, 3])
-    Pixels.fill(0)
+    Pixels.fill(255)
     Oog = [float(oogx), float(oogy), float(oogz)]
     Scherm = [0, 0, -2]
     SchermCor = [0, 0]
@@ -27,7 +27,6 @@ def NewImg(xt, yt, zt, punten, oogx, oogy, oogz):
     for i in [0, 1]:
         SchermCor[i] = s*(0-Oog[i]) + Oog[i]
 
-    tic=timeit.default_timer()
     for t in np.linspace(0, 2*np.pi, (int(punten)+1)):
         p[0] = eval(xtc)
         p[1] = eval(ytc)
@@ -45,23 +44,11 @@ def NewImg(xt, yt, zt, punten, oogx, oogy, oogz):
         else:
             Baky.append('skip')
         # print(str(round((timeit.default_timer()-tic)*1000, 8))+ 'ms (Pixels)')
-    print(str(round((timeit.default_timer()-tic)*1000, 8))+ 'ms (Calculating)')
 
-    tic=timeit.default_timer()
     Pixels = PixCalc(ImgHeight, ImgWidth, Bakx, Baky, Pixels, 0, 0, 0)
-    print(str(round((timeit.default_timer()-tic)*1000, 8))+ 'ms (Pixels)')
 
-    Scale = 1
-    tic=timeit.default_timer()
-    # array = np.array(Pixels, dtype=np.uint8)
-    # new_image = Image.fromarray(array)
-    # new_image.save('static/new.png')
     array = np.array(Pixels, dtype=np.uint8)
     new_image = Image.fromarray(array)
     
-    print(str(round((timeit.default_timer()-tic)*1000, 8))+ 'ms (Constructing Image)')
-    
     print(str(round((timeit.default_timer()-tictot)*1000, 8))+ 'ms (Total)')
     return new_image
-    # plt.plot(xCoordsScaled, yCoordsScaled)
-    # plt.show()
